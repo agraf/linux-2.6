8dcc4e4fdcd9c92ac5524f515031589f0f2f7caf
Alexander Graf <agraf@suse.de>
2012-02-27 03:33:32 +0100

    XXX fix stwu

--- arch/powerpc/kvm/emulate.c
+++ arch/powerpc/kvm/emulate.c
@@ -185,7 +185,7 @@ static int kvmppc_emulate_stw(struct kvm_vcpu *vcpu, int rs, int ra, int d)
 	return kvmppc_handle_store(vcpu->run, vcpu, val, 4, 1);
 }
 
-static int kvmppc_emulate_stwu(struct kvm_vcpu *vcpu, int rt, int ra, int d)
+static int kvmppc_emulate_stwu(struct kvm_vcpu *vcpu, int rs, int ra, int d)
 {
 	int r;
 	ulong val = kvmppc_get_gpr(vcpu, rs);
