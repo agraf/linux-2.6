6b99d9d2c47a4dfc117ed42a3ef7cfe2af0f1719
Alexander Graf <agraf@suse.de>
2012-02-27 05:00:47 +0100

    XXX x form

--- arch/powerpc/include/asm/kvm_ppc.h
+++ arch/powerpc/include/asm/kvm_ppc.h
@@ -59,7 +59,7 @@ extern void kvmppc_emulate_init(void);
 extern void kvmppc_emulate_exit(void);
 extern void __init kvmppc_emulate_register_d(int op, int flags,
                 int (*func)(struct kvm_vcpu *vcpu, int rt, int ra, int d));
-extern void __init kvmppc_emulate_register_x(int op, int flags,
+extern void kvmppc_emulate_register_x(int op, int flags,
 	int (*func)(struct kvm_vcpu *vcpu, int rt, int ra, int rb, int rc));
 
 extern int kvmppc_vcpu_run(struct kvm_run *kvm_run, struct kvm_vcpu *vcpu);
